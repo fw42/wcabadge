050e81e0ef25ce2bff3152ea6084ad7db67c5c43
Florian Weingarten <flo@hackvalue.de>
2012-09-16 23:12:00 +0200

    foo

--- mysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysql = new mysqli("localhost", "MyUsername", "MyPassword", "MyUsername");
-
-if(mysqli_connect_errno()) {
-	die(sprintf("MySQL connection failed: %s\n", mysqli_connect_error()));
-}
-?>
