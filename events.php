23bb865308a257504f8db23fea7f514b2c0c5576
Florian Weingarten <flo@hackvalue.de>
2014-04-27 22:11:36 +0200

    add skewb

--- events.php
+++ events.php
@@ -3,6 +3,6 @@
 $events = array(
 	"333", "444", "555", "222", "333bf", "333oh", "333fm", "333ft",
 	"minx", "pyram", "sq1", "clock", "666", "777", "magic", "mmagic",
-	"444bf", "555bf", "333mbf"
+	"444bf", "555bf", "333mbf", "skewb"
 );
 ?>
